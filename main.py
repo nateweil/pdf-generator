9649ddf542f8113bb5d31903aee893d3d04bcdee
natew <nateweil1993@gmail.com>
2023-01-21 09:47:18 -0500

    Add footers with page numbers

--- main.py
+++ main.py
@@ -5,19 +5,31 @@ import pandas as pandas
 data = pandas.read_csv("topics.csv")
 
 pdf = FPDF(orientation="portrait", unit="mm", format="letter")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in data.iterrows():
+    # Just three variable assignments, stacked together for fun.
     order, topic, pages = row["Order"], row["Topic"], row["Pages"]
 
-    i=0
-    while i < pages:
+    # I initially used "while i < pages" and incremented i, but this loop is neater.
+    for i in range(pages):
         pdf.add_page()
-        pdf.set_font(family="Times", style="B", size=24)
-        pdf.set_text_color(175, 0, 0)
-        pdf.cell(w=0, h=12, txt=topic, align="L", ln=1)
-        pdf.line(x1=11, y1=21, x2=200, y2=21)
-        i+=1
 
+        if i == 0:
+            # Add the header to just the first page of each topic
+            pdf.set_font(family="Times", style="B", size=24)
+            pdf.set_text_color(175, 0, 0)
+            pdf.cell(w=0, h=12, txt=topic, align="L", ln=1)
+            pdf.line(x1=11, y1=21, x2=200, y2=21)
+
+            # pdf.ln adds break lines
+            pdf.ln(240)
+        else: # If it's a blank page, needs more break lines
+            pdf.ln(251)
+
+        pdf.set_font(family="Times", style="i", size=10)
+        pdf.set_text_color(150,150,150)
+        pdf.cell(w=0, h=10, txt=f"{topic} {str(i+1)}", align="R")
 
 pdf.output("output.pdf")
 
